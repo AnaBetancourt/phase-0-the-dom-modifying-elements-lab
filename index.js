407e36d4996e8f82b70ca0d6895c976d7d7d4d50
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-03-16 22:27:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "Ana is the champion";
